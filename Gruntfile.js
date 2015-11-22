fc1aed9615515309dbd840320b6819a7a8815182
alexkonovalov <alvkonovalov@gmail.com>
2015-11-22 16:46:40 +0300

    added processhtml

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,24 @@
 module.exports = function(grunt) {
 
     var globalConfig = {
-        source: 'src',
         output: 'dist',
+        source: 'src',
         sourceScripts: "src/scripts",
         sourceStyles: "src/styles",
-        scriptsCompiledFileName: "scripts",
-        stylesCompiledFileName: "styles"
+        scriptsDestFileName: "scripts",
+        stylesDestFileName: "styles"
     };
 
     grunt.initConfig({
         globalConfig: globalConfig,
+        processhtml: {
+            dist: {
+
+                files: {
+                    '<%= globalConfig.output %>/index.html': '<%= globalConfig.source %>/index.html'
+                }
+            }
+        },
         bowercopy: {
             options: {
                 srcPrefix: "bower_components"
@@ -25,7 +33,7 @@ module.exports = function(grunt) {
                     "d3.min.js": "d3/d3.min.js",
                     "jquery.min.js": "jquery/dist/jquery.min.js",
                     "bootstrap.min.js": "bootstrap/dist/js/bootstrap.min.js",
-                    "cosmo.min.css": "bootstrap-theme-cosmo/cosmo.min.css",
+                    "cosmo.min.css": "bootstrap-theme-cosmo/cosmo.min.css"
                 }
             }
         },
@@ -35,32 +43,24 @@ module.exports = function(grunt) {
             },
             src: ['**']
         },
-        copy: {
-            files: {
-                cwd: "src",
-                src: "index.html",
-                dest: "<%= globalConfig.output %>",
-                expand: true
-            }
-        },
         concat: {
             jstarget: {
                 options: {
-                    separator: ";\n",
+                    separator: ";\n"
                 },
                 src: [  "<%= globalConfig.sourceScripts %>/app.js",
                         "<%= globalConfig.sourceScripts %>/dataProviderService.js",
                         "<%= globalConfig.sourceScripts %>/stockChartController.js",
                         "<%= globalConfig.sourceScripts %>/lineChartDirective.js"],
-                dest: "<%= globalConfig.output %>/<%= globalConfig.scriptsCompiledFileName %>.js",
+                dest: "<%= globalConfig.output %>/<%= globalConfig.scriptsDestFileName %>.js"
             },
             csstarget: {
                 options: {
-                    separator: "\n",
+                    separator: "\n"
                 },
                 src: ["<%= globalConfig.sourceStyles %>/main.css", "<%= globalConfig.sourceStyles %>/lineChart.css"],
-                dest: "<%= globalConfig.output %>/<%= globalConfig.stylesCompiledFileName %>.css",
-            },
+                dest: "<%= globalConfig.output %>/<%= globalConfig.stylesDestFileName %>.css"
+            }
         },
         uglify: {
             options: {
@@ -69,19 +69,20 @@ module.exports = function(grunt) {
                 sourceMap: true
             },
             jstarget: {
-                src: "<%= globalConfig.output %>/<%= globalConfig.scriptsCompiledFileName %>.js",
-                dest: "<%= globalConfig.output %>/<%= globalConfig.scriptsCompiledFileName %>.min.js"
+                src: "<%= globalConfig.output %>/<%= globalConfig.scriptsDestFileName %>.js",
+                dest: "<%= globalConfig.output %>/<%= globalConfig.scriptsDestFileName %>.min.js"
             }
         }
     });
 
+
     grunt.loadNpmTasks("grunt-gh-pages");
-    grunt.loadNpmTasks("grunt-copy");
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-bowercopy");
+    grunt.loadNpmTasks("grunt-processhtml");
 
-    grunt.registerTask("default", ["concat", "uglify", "copy", "bowercopy"]);
-    grunt.registerTask("publish", ["concat", "uglify", "copy", "bowercopy", "gh-pages"])
+    grunt.registerTask("default", ["concat", "uglify", "bowercopy", "processhtml"]);
+    grunt.registerTask("publish", ["default", "gh-pages"])
 
 };
